eef0a32e2fc3577cbea6ae5b5b6ece6eccf91e13
gkshtz <gkshitiz21@gmail.com>
2024-06-09 17:45:02 +0530

    get and post APIs

---
+++ TaskManagement/Models/TaskModel.cs
@@ -0,0 +1,19 @@
+﻿using TaskManagement.Utils.Enums;
+
+namespace TaskManagement.Models
+{
+    public class TaskModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        public Status Status { get; set; }
+    }
+}
